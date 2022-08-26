d94938099ce7838f0621c54a3b777d6fe6bdb473
yurikgs <yurikgs@outlook.com>
2022-08-26 17:40:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require("express")
+const app = express() 
+
+
+app.get("/", (req, res) => {
+
+    res.send("Hello World! + Olá Mundo! :)")
+})
+
+
+
+
+app.listen(3000, () => console.log("App rodando!"))
+
+
